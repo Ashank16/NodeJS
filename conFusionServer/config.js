89f927088c48e3b5247d55e4c362a8a9507dccac
Ashank16 <ashankjuyal@gmail.com>
2020-09-24 00:25:38 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '652385175416898',
+        clientSecret: '75b521617da2a0ebfc3b631b63cdbc94'
+    }    
 }
